64084ea627331b91d156d507b263b81ac2cbcf96
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-08 18:06:41 +0200

    Fixed bugs

---
+++ src/utils/queenbee/getAndSendRoles.ts
@@ -0,0 +1,54 @@
+import { Client, Role } from "discord.js"
+import config from "../config.js"
+
+const tekkomBotApiUrl = config.tekkomBotApiUrl
+const tekkomBotApiToken = config.tekkomBotApiToken
+const LOGIN_GUILD = "284789429539700736"
+
+/**
+ * Fetches all channels the bot can write to in the 'Login - Linjeforeningen for IT' server
+ * @param client Discord client
+ * @returns void
+ */
+export default async function getAndSendRoles(client: Client): Promise<void> {
+    const GUILD_ID = LOGIN_GUILD
+    const data: any[] = []
+
+    try {
+        const guild = client.guilds.cache.get(GUILD_ID)
+        if (!guild) {
+            console.warn(`Bot is not in guild with ID ${GUILD_ID}`)
+            return
+        }
+
+        const roles = await guild.roles.fetch()
+        roles.forEach((role: Role) => {
+            if (role.name === '@everyone' || role.id === '284789429539700736') {
+                return
+            }
+
+            data.push({
+                name: role.name,
+                id: role.id
+            })
+        })
+
+        const response = await fetch(`${tekkomBotApiUrl}/roles`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${tekkomBotApiToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (!response.ok) {
+            throw new Error(await response.text())
+        }
+
+        const result = await response.json() as object
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
--- src/utils/queenbee/queenbeeMonitor.ts
+++ src/utils/queenbee/queenbeeMonitor.ts
@@ -4,10 +4,12 @@ import getAndSendChannels from "./getAndSendChannels.js"
 import getMessages from "./getMessages.js"
 import sendMessages from "./sendMessages.js"
 import updateApi from "./updateApi.js"
+import getAndSendRoles from "./getAndSendRoles.js"
 
 export default async function queenbeeMonitor(client: Client) {
     schedule('* * * * *', async() => {
         getAndSendChannels(client)
+        getAndSendRoles(client)
         const messages = await getMessages()
         const result = await sendMessages(client, messages)
         await updateApi(result)
