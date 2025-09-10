1dd867507f4d9e522a6f98bc55c1ce878171310a
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-10 22:42:53 +0200

    queenbee -> queenbee-btg

--- api/src/handlers/login/getToken.ts
+++ api/src/handlers/login/getToken.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import tokenWrapper from "@utils/tokenWrapper.js"
 
 export default async function getToken(req: FastifyRequest, res: FastifyReply) {
-    const response = await tokenWrapper(req, res, ['tekkom-bot', 'queenbee'])
+    const response = await tokenWrapper(req, res, ['tekkom-bot', 'queenbee-btg'])
     if (!response.valid) {
         return res.status(400).send(response)
     }
