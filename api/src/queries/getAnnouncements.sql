1d9994d6ffbd12fd5fd4fdb996dd43455e3380b8
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-17 23:49:49 +0200

    fixed bug

--- api/src/queries/getAnnouncements.sql
+++ api/src/queries/getAnnouncements.sql
@@ -1,6 +1,6 @@
 SELECT * FROM announcements
-WHERE ($3::BOOLEAN IS NULL OR interval IS NOT NULL OR sent = false)
-  AND ($4::BOOLEAN IS NULL OR (sent = false AND (time IS NULL OR time <= NOW())))
+WHERE (COALESCE($3::BOOLEAN, false) = false OR interval IS NOT NULL OR sent = false)
+  AND (COALESCE($4::BOOLEAN, false) = false OR (sent = false AND (time IS NULL OR time <= NOW())))
 ORDER BY id DESC
 LIMIT $2::INT
 OFFSET (($1::INT * $2::INT) - $2::INT);
