1cd79a43bc18f86034a1a579d54862eba126bac5
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-19 17:50:21 +0200

    fixed incorrect timestamp

--- api/src/queries/getAverageDurationPerSong.sql
+++ api/src/queries/getAverageDurationPerSong.sql
@@ -1,20 +1,13 @@
 SELECT
-    -- Average song duration in seconds
     ROUND(AVG(EXTRACT(EPOCH FROM ("end" - "start"))))::INT AS avg_seconds,
-
-    -- Total duration in minutes
-    ROUND(SUM(EXTRACT(EPOCH FROM ("end" - "start"))) / 60)::INT AS total_minutes,
-
-    -- Total duration in minutes this year
+    ROUND(SUM(EXTRACT(EPOCH FROM ("end" - "start")) / 60.0))::INT AS total_minutes,
     ROUND(
         SUM(
             CASE WHEN DATE_PART('year', "start") = DATE_PART('year', CURRENT_DATE)
-                 THEN EXTRACT(EPOCH FROM ("end" - "start"))
+                 THEN EXTRACT(EPOCH FROM ("end" - "start")) / 60.0
                  ELSE 0
             END
-        ) / 60
+        )
     )::INT AS total_minutes_this_year,
-
-    -- Total number of songs played
     COUNT(*) AS total_songs
 FROM activities;
