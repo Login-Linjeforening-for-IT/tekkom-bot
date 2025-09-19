326bb0c1697fa84dfa1015e32ba9dc04fd8649ea
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-19 18:57:55 +0200

    fixed bugs

--- api/src/queries/getMostLikedAlbums.sql
+++ api/src/queries/getMostLikedAlbums.sql
@@ -1,12 +1,13 @@
 SELECT 
     a.album,
     a.artist,
+    s."image",
     SUM(s.listens) AS total_listens,
     SUM(s.skips) AS total_skips,
     (SUM(s.listens)::float / NULLIF(SUM(s.listens + s.skips), 0)) AS like_ratio
 FROM songs s
 JOIN activities a
   ON s.name = a.song AND s.artist = a.artist AND s.album = a.album
-GROUP BY a.album, a.artist
+GROUP BY a.album, a.artist, s."image"
 ORDER BY like_ratio DESC
 LIMIT 5;
--- api/src/queries/getMostLikedArtists.sql
+++ api/src/queries/getMostLikedArtists.sql
@@ -1,11 +1,12 @@
 SELECT 
     a.artist,
+    s."image",
     SUM(s.listens) AS total_listens,
     SUM(s.skips) AS total_skips,
     (SUM(s.listens)::float / NULLIF(SUM(s.listens + s.skips), 0)) AS like_ratio
 FROM songs s
 JOIN activities a
   ON s.name = a.song AND s.artist = a.artist
-GROUP BY a.artist
+GROUP BY a.artist, s."image"
 ORDER BY like_ratio DESC
 LIMIT 5;
