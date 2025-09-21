cb82f40b0b198abf5cd1d0a0425a51c46e9bce2f
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-21 16:23:20 +0200

    sorted by time played

--- api/src/queries/getCurrentlyPlaying.sql
+++ api/src/queries/getCurrentlyPlaying.sql
@@ -1,12 +1,15 @@
-SELECT DISTINCT ON (a.user_id)
-    a.*,
-    s."image",
-    s.listens AS listens
-FROM activities a
-LEFT JOIN songs s
-  ON a.song = s.name
-  AND a.artist = s.artist
-WHERE a."start" <= NOW()
-  AND a."end" >= NOW()
-  AND NOT a.skipped
-ORDER BY a.user_id, a."start" DESC;
+SELECT * FROM (
+    SELECT DISTINCT ON (a.user_id)
+        a.*,
+        s."image",
+        s.listens AS listens
+    FROM activities a
+    LEFT JOIN songs s
+      ON a.song = s.name
+      AND a.artist = s.artist
+    WHERE a."start" <= NOW()
+      AND a."end" >= NOW()
+      AND NOT a.skipped
+    ORDER BY a.user_id, a."start" DESC
+) AS per_user
+ORDER BY (NOW() - per_user."start") ASC;
