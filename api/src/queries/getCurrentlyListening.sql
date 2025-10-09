9dcb4799bbf27c170b60e21115d47ecddfeca447
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-09 22:48:43 +0200

    v2.12.3: Added missing fields

--- api/src/queries/getCurrentlyListening.sql
+++ api/src/queries/getCurrentlyListening.sql
@@ -1,10 +1,14 @@
 SELECT * FROM (
     SELECT DISTINCT ON (l.user_id)
         l.*,
-        s."image"
+        s."image",
+        s.name,
+        ar.name AS artist,
+        al.name AS album
     FROM listens l
-    LEFT JOIN songs s
-      ON l.song_id = s.id
+    LEFT JOIN songs s ON l.song_id = s.id
+    LEFT JOIN artists ar ON s.artist = ar.id
+    LEFT JOIN albums al ON s.album = al.id
     WHERE l."start" <= NOW()
       AND l."end" >= NOW()
       AND NOT l.skipped
