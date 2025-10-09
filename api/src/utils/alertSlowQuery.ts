002bcfbd9d52091c3366eb850184dc8d0a0b8014
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-09 21:41:30 +0200

    v2.11.1: Changed ping to critical role dev

--- api/src/utils/alertSlowQuery.ts
+++ api/src/utils/alertSlowQuery.ts
@@ -21,7 +21,7 @@ export default async function alertSlowQuery(duration: number, name: string) {
         }
         
         if (duration > (config.CACHE_TTL - 1)) {
-            data.content = `🚨 <@&${config.CRITICAL_ROLE}> 🚨`
+            data.content = `🚨 <@&${config.CRITICAL_ROLE_DEV}> 🚨`
         }
 
         console.warn(`${firstUpperCaseName} query exceeded half of cache TTL: ${duration.toFixed(2)}s`)
