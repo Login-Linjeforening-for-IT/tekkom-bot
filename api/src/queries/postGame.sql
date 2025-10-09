304c1b11748cfba597df918d6763ab752feea8ac
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-09 13:31:20 +0200

    Fixed postgame

--- api/src/queries/postGame.sql
+++ api/src/queries/postGame.sql
@@ -1,7 +1,25 @@
 INSERT INTO games (name, image, image_text)
 VALUES ($1, $2, $3)
-ON CONFLICT (name) 
-DO UPDATE SET
-    players = games.players + 1,
+ON CONFLICT (name) DO UPDATE SET
     image = EXCLUDED.image,
-    image_text = EXCLUDED.image_text;
+    image_text = EXCLUDED.image_text
+RETURNING id;
+
+INSERT INTO game_activity (
+    game_id,
+    user_id,
+    details,
+    state,
+    application,
+    start_time,
+    party
+)
+VALUES (
+    (SELECT id FROM games WHERE name = $1),
+    $3,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9
+);
