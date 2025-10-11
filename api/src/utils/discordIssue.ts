b8dd8c435ca8d4ee57ca8fed7be9d454ddcd3c4b
0herm <0herm@protonmail.com>
2025-10-11 12:08:14 +0200

    fix name and color

--- api/src/utils/discordIssue.ts
+++ api/src/utils/discordIssue.ts
@@ -1,15 +1,15 @@
-import config from "#constants"
+import config from '#constants'
 
 const { WEBHOOK_URL_ISSUE } = config
 
-export default async function discordIssue(title: string, description: string, footer: string) {
+export default async function discordIssue(title: string, description: string, footer: string, color: string) {
     try {
         let data: { content?: string; embeds: any[] } = {
             embeds: [
                 {
                     title: title,
                     description: description,
-                    color: 0xff0000,
+                    color: getColor(color),
                     timestamp: new Date().toISOString(),
                     footer: footer ? { text: footer } : undefined
                 }
@@ -31,5 +31,27 @@ export default async function discordIssue(title: string, description: string, f
         return response.status
     } catch (error) {
         console.log(error)
+        throw error
+    }
+}
+
+function getColor(name: string) {
+    switch (name.toLowerCase()) {
+        case 'blue':
+            return '#4493f8'
+        case 'green':
+            return '#3fb950'
+        case 'yellow':
+            return '#d29922'
+        case 'orange':
+            return '#db6d28'
+        case 'red':
+            return '#f85149'
+        case 'pink':
+            return '#db61a2'
+        case 'purple':
+            return '#ab7df8'
+        default:
+            return '#9198a1'
     }
 }
